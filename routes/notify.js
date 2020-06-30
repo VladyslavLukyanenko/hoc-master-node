5a2299088a5db232d258fffa4e783bd77444f7ec
Vladyslav Lukyanenko <me@vladyslav.xyz>
2020-07-01 00:58:11 +0200

    Remove storing personal data
    Signed-off-by: Vladyslav Lukyanenko <me@vladyslav.xyz>

--- routes/notify.js
+++ routes/notify.js
@@ -5,7 +5,6 @@ const sendWebhook = require('../actions/sendWebhook');
 const notifyNodes = require('../actions/notifyNodes');
 const mongoose = require('mongoose');
 const Node = mongoose.model('Node');
-const Clown = mongoose.model('Clown');
 
 router.post('/', async function (request, response) {
 const { customer_email_address, customer_purchase_ip, cards, reason, secret} = request.body;
@@ -20,10 +19,6 @@ const payload = {
     cards,
     reason: "Fraud"
 };
-const clown = await Clown.findOne({
-    Email: customer_email_address
-});
-if(!clown) await addClown(payload);
 sendWebhook({...payload, reason, node});
 notifyNodes(payload);
 return response.status(200).json({success: true});
--- schemas/Clown.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Clown = new Schema({
-    Email: String,
-    IP: String,
-    Cards: [String]
-});
-module.exports = mongoose.model("Clown", Clown);

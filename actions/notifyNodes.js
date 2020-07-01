cf215e2a2e0bcd9d91b4170d09e850357377fa7c
Vladyslav Lukyanenko <me@vladyslav.xyz>
2020-07-01 20:58:26 +0200

    Try catch on node notification
    Signed-off-by: Vladyslav Lukyanenko <me@vladyslav.xyz>

--- actions/notifyNodes.js
+++ actions/notifyNodes.js
@@ -4,5 +4,11 @@ const axios = require('axios');
 
 module.exports.notify = async function(payload){
     const nodes = await Node.find();
-    nodes.forEach(async (node) => await axios.post(`${node.URI}/api/execute`, {...payload, secret: node.Secret}))
+    nodes.forEach(async (node) => {
+    try {
+            await axios.post(`${node.URI}/api/execute`, {...payload, secret: node.Secret})
+    } catch (error) {
+        
+    }
+})
 };
